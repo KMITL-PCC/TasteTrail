44be110a4704bd23b43b79c98a8f070f2465ea79
zeroznice <zeroznice.nice@gmail.com>
2025-09-27 15:11:54 +0700

    UpdateRestEdit

---
+++ frontend/app/restaurants/edit/page.tsx
@@ -0,0 +1,7 @@
+import RestaurantEditForm from "@/components/store/RestaurantEditForm";
+
+const Restaurantcreatepage = () => {
+  return <RestaurantEditForm />;
+};
+
+export default Restaurantcreatepage;
